851fabee5de9126d3fa82eb2efe4da6716c916eb
Kush Shah <kushshah@Kushs-MacBook-Pro.local>
2023-03-15 17:50:02 -0600

    final commit

--- src/TrimDropdown.js
+++ src/TrimDropdown.js
@@ -1,42 +1,39 @@
-import React, {useContext} from "react";
-import Box from '@mui/material/Box';
-import InputLabel from '@mui/material/InputLabel';
-import MenuItem from '@mui/material/MenuItem';
-import FormControl from '@mui/material/FormControl';
-import Select from '@mui/material/Select';
-import CarContext from './CarContext';
+import React, { useContext } from "react";
+import Box from "@mui/material/Box";
+import InputLabel from "@mui/material/InputLabel";
+import MenuItem from "@mui/material/MenuItem";
+import FormControl from "@mui/material/FormControl";
+import Select from "@mui/material/Select";
+import CarContext from "./CarContext";
 
 const TrimDropdown = () => {
+  const [age, setAge] = React.useState("");
 
-    const [age, setAge] = React.useState('');
+  const { addToTrim } = useContext(CarContext);
 
-    const { addToTrim } = useContext(CarContext);
+  const handleChange = (event) => {
+    addToTrim(event.target.value);
+    setAge(event.target.value);
+    // comment
+  };
 
-    const handleChange = (event) => {
-        addToTrim(event.target.value)
-        setAge(event.target.value);
-      };
-  
-      
-
-
-
-
-    return (<Box sx={{ minWidth: 130 }}>
-        <FormControl fullWidth>
-          <InputLabel id="demo-simple-select-label">Trim</InputLabel>
-          <Select
-            labelId="demo-simple-select-label"
-            id="demo-simple-select"
-            value={age}
-            label="Trim"
-            onChange={handleChange}
-          > 
-            <MenuItem value={20}>SR5 Premium 4WD</MenuItem>
-            {/* <MenuItem value={30}>Thirty</MenuItem> */}
-          </Select>
-        </FormControl>
-      </Box>)
-}
+  return (
+    <Box sx={{ minWidth: 130 }}>
+      <FormControl fullWidth>
+        <InputLabel id="demo-simple-select-label">Trim</InputLabel>
+        <Select
+          labelId="demo-simple-select-label"
+          id="demo-simple-select"
+          value={age}
+          label="Trim"
+          onChange={handleChange}
+        >
+          <MenuItem value={20}>SR5 Premium 4WD</MenuItem>
+          {/* <MenuItem value={30}>Thirty</MenuItem> */}
+        </Select>
+      </FormControl>
+    </Box>
+  );
+};
 
 export default TrimDropdown;
