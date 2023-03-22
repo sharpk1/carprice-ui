9f6a101b133fa21a627be1b4efd4ec9595677aee
Kush Shah <kushshah@Kushs-MacBook-Pro.local>
2023-03-21 18:25:26 -0600

    remove Submit t

--- src/HomePage.js
+++ src/HomePage.js
@@ -1,12 +1,14 @@
 import React from "react";
 import Button from "@mui/material/Button";
-import {Link} from "react-router-dom";
+import { Link } from "react-router-dom";
 
 const HomePage = () => {
   return (
     <>
-      <Button variant="contained" component={Link} to="/search">Search</Button>
-      <Button variant="contained">Submittt</Button>
+      <Button variant="contained" component={Link} to="/search">
+        Search
+      </Button>
+      <Button variant="contained">Submi</Button>
     </>
   );
 };
